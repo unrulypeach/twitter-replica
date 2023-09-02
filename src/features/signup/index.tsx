cea1cebc8e7b43bf02b8fbafbbb8a792ae39ec40
lc <gingeraleisthebest@gmail.com>
2023-09-02 13:57:06 -0400

    ran linter

--- src/features/signup/index.tsx
+++ src/features/signup/index.tsx
@@ -1,17 +1,15 @@
-import SignupGoogleBtn from "./signupGoogleBtn";
-import SignupAppleBtn from "./signupAppleBtn";
-import SignupCreateAccount from "./signupCreateAcc";
-import SignupTos from "./signupTos";
-import { Link } from "react-router-dom";
+import SignupGoogleBtn from './signupGoogleBtn';
+import SignupAppleBtn from './signupAppleBtn';
+import SignupCreateAccount from './signupCreateAcc';
+import SignupTos from './signupTos';
+import { Link } from 'react-router-dom';
 
 export default function SignUp(): JSX.Element {
   return (
     <div className="mb-[16px] border-[1px] border-searchbar rounded-[16px]">
       <div className="flex flex-col">
         <div className="px-[16px] py-[12px]">
-          <span className="text-[20px] leading-[24px] font-extrabold">
-            New to Twitter?
-          </span>
+          <span className="text-[20px] leading-[24px] font-extrabold">New to Twitter?</span>
         </div>
 
         <div className="px-[12px]">
--- src/features/signupModal/index.tsx
+++ src/features/signupModal/index.tsx
@@ -1,10 +1,10 @@
-import { twitterBirdBlue } from "../../styles/assets/icons/iconData";
-import SignupAppleBtn from "../signup/signupAppleBtn";
-import SignupGoogleBtn from "../signup/signupGoogleBtn";
-import SignupCreateAccountDark from "../signup/signupCreateAccDark";
-import SignupTos from "../signup/signupTos";
-import CloseModal from "./closeModal";
-import { Link } from "react-router-dom";
+import { twitterBirdBlue } from '../../styles/assets/icons/iconData';
+import SignupAppleBtn from '../signup/signupAppleBtn';
+import SignupGoogleBtn from '../signup/signupGoogleBtn';
+import SignupCreateAccountDark from '../signup/signupCreateAccDark';
+import SignupTos from '../signup/signupTos';
+import CloseModal from './closeModal';
+import { Link } from 'react-router-dom';
 
 export default function SignupModal(): JSX.Element {
   return (
@@ -19,9 +19,7 @@ export default function SignupModal(): JSX.Element {
       </div>
 
       <div className="my-[19px]">
-        <span className="text-[29px] leading-[34px] font-bold">
-          Join Twitter today
-        </span>
+        <span className="text-[29px] leading-[34px] font-bold">Join Twitter today</span>
       </div>
 
       <div className="flex flex-col px-[30px] max-w-[364px] min-w-[364px] m-auto">
@@ -49,7 +47,7 @@ export default function SignupModal(): JSX.Element {
 
         <div className="mt-[38px]">
           <span className="text-[14px] leading-[19px]">
-            Have an account already?{" "}
+            Have an account already?{' '}
             <Link to="/login" className="text-blue">
               Log in
             </Link>
