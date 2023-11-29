591ea035981278d39d7f472a01d0d6d9fce5c679
lc <gingeraleisthebest@gmail.com>
2023-11-29 18:28:55 -0500

    updated options

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,4 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: 'http://localhost:8000/api',
+  headers: { 'Content-Type': 'application/json' },
+  withCredentials: true,
 });
