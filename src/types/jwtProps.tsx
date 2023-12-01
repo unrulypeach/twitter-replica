73a58c3088b08f67407e1c89a4a8932d268b3064
lc <gingeraleisthebest@gmail.com>
2023-12-01 17:14:27 -0500

    added 2ndary tweet type

--- src/types/jwtProps.tsx
+++
@@ -1,7 +0,0 @@
-import userProps from './userProps';
-
-/* declare module 'jsonwebtoken' {
-  export interface JwtPaylod {
-    user: userProps;
-  }
-} */
--- src/types/tweetProps.tsx
+++ src/types/tweetProps.tsx
@@ -6,11 +6,20 @@ interface ShortUserProps {
   userhandle: string;
   profile_pic: string;
 }
-export default interface TweetProps {
+export interface TweetProps {
   _id: Types.ObjectId;
   content: string;
   uid: ShortUserProps;
   comments: Array<Types.ObjectId>;
+  likes: Array<Types.ObjectId>;
+  date: string;
+}
+
+export interface TweetPopulatedCommentsProps {
+  _id: Types.ObjectId;
+  content: string;
+  uid: ShortUserProps;
+  comments: Array<TweetProps>;
   likes: Array<Types.ObjectId>;
   date: string;
 }
