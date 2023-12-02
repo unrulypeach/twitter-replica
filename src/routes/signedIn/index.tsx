103a75d23ec3fad930281dab766e3887b005ebef
lc <gingeraleisthebest@gmail.com>
2023-12-02 01:21:29 -0500

    removed comments

--- src/routes/signedIn/index.tsx
+++ src/routes/signedIn/index.tsx
@@ -5,10 +5,9 @@ import NoHome from '../../components/noContent/noHome';
 import { useAuthContext } from '../../contexts/authContext';
 import Tweet from '../../features/tweet';
 import { useState, useEffect } from 'react';
-import { getHomePosts } from '../../services/firebase/firestore';
 import SignedInRSideMenu from '../../components/sidemenu/right/signedInRSideMenu';
 import axios from '../../api/axios';
-import TweetProps from '../../types/tweetProps';
+import type { TweetProps } from '../../types/tweetProps';
 
 export default function SignedInHome(): JSX.Element {
   const [posts, setPosts] = useState<JSX.Element[]>([]);
@@ -17,11 +16,9 @@ export default function SignedInHome(): JSX.Element {
   const { userProfile } = useAuthContext();
   useEffect(() => {
     const postz = async (): Promise<void> => {
-      // const userhandleOrError = userProfile?.userhandle ?? 'ERROR';
       if (!userProfile?.userhandle) {
         return;
       }
-      // const dlPosts = await getHomePosts();
       try {
         const postRes = await axios.get<TweetProps[]>('/post/homepage');
         const mappedPosts: Array<JSX.Element> = postRes.data.map((post: TweetProps) => {
@@ -31,9 +28,6 @@ export default function SignedInHome(): JSX.Element {
               _id={post._id}
               uid={post.uid}
               content={post.content}
-              /* username={post?.uid.username}
-              userhandle={post?.uid.userhandle}
-              userPic={post?.userPic ?? ''} */
               // imgLink={post?.profile_pic ?? ''}
               // path={post?.path}
               date={post.date}
@@ -43,7 +37,6 @@ export default function SignedInHome(): JSX.Element {
           );
         });
         setPosts(mappedPosts);
-        console.log('home posts fetched:', postRes);
       } catch (err) {
         console.error(err);
       }
