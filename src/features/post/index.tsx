2c8977304290d1e49300a0569573a0b0868f933e
lc <gingeraleisthebest@gmail.com>
2023-12-07 23:13:34 -0500

    updated to use api method and fixed btn to allow submitting before trim

--- src/features/post/index.tsx
+++ src/features/post/index.tsx
@@ -1,23 +1,31 @@
 import { useState, useRef, useEffect } from 'react';
 import Avatar from '../../components/user/avatar';
 import { useAuthContext } from '../../contexts/authContext';
+import useAxiosPrivate from '../../hooks/useAxiosInterceptors';
 import { dropdown, emoji, gif, globe, location, media, poll, schedule } from '../../styles/assets/icons/iconData';
-import { post } from '../../services/firebase/firestore';
+import { handleAxiosValidationError } from '../../scripts/errorHandling';
 
+interface PostProps {
+  setShouldUpdate: React.Dispatch<React.SetStateAction<boolean>>;
+}
 // setPosts is received only from <SignedInHome />
-export default function Post({ setShouldUpdate }): JSX.Element {
+export default function Post({ setShouldUpdate }: PostProps): JSX.Element {
+  const axiosPrivate = useAxiosPrivate();
   const { userProfile } = useAuthContext();
   const [postValue, setPostValue] = useState('');
   const [imgFile, setImgFile] = useState<File | undefined>(undefined);
   const [previewImg, setPreviewImg] = useState<string | undefined>();
-  const textareaRef = useRef(null);
+  const textareaRef = useRef();
 
+  // changes text box height when typing instead of
+  // default behavior where you have to scroll
   useEffect(() => {
     textareaRef.current.style.height = '0px';
     const scrollHeight = textareaRef.current.scrollHeight;
     textareaRef.current.style.height = scrollHeight + 'px';
   }, [postValue]);
 
+  // sets the selected image for preview
   useEffect(() => {
     if (!imgFile) {
       setPreviewImg(undefined);
@@ -28,24 +36,24 @@ export default function Post({ setShouldUpdate }): JSX.Element {
     return () => URL.revokeObjectURL(objectUrl);
   }, [imgFile]);
 
-  const handlePost = () => {
-    if (userProfile?.userhandle) {
-      if (imgFile)
-        void post(userProfile?.userhandle, userProfile?.username, userProfile?.profile_pic, postValue, imgFile).then(
-          () => {
-            setPostValue('');
-            setImgFile(undefined);
-            setPreviewImg(undefined);
-            setShouldUpdate((prev) => !prev);
-          },
-        );
-      void post(userProfile?.userhandle, userProfile?.username, userProfile?.profile_pic, postValue).then(() => {
+  const handlePost = async () => {
+    // need to upload pic to firestore first and get link
+    const config = {
+      ...(postValue && { content: postValue }),
+      // ...(imgLink) && {image: imgLink},
+    };
+    try {
+      const uploadPost = await axiosPrivate.post('/post/create', config);
+      if (uploadPost.status === 200) {
         setPostValue('');
-        setImgFile(undefined);
-        setPreviewImg(undefined);
-        setShouldUpdate((prev) => !prev);
-      });
+        // setImgFile(undefined);
+        // setPreviewImg(undefined);
+        setShouldUpdate((prev) => !prev); // is there a way to just push tweet to stack
+      }
+    } catch (error) {
+      handleAxiosValidationError(error);
     }
+
     // add post to feed
   };
 
@@ -56,12 +64,12 @@ export default function Post({ setShouldUpdate }): JSX.Element {
       </div>
       <div className="grow">
         <div className="flex flex-col">
-          <div className="flex flex-row pt-[4px] pb-[11px]">
+          {/* <div className="flex flex-row pt-[4px] pb-[11px]">
             <div className="flex flex-row items-center border-[1px] border-dark-650 rounded-full text-[13px] leading-[15px] px-[11px] min-w-[23px] min-h-[23px]">
               <span className="text-blue font-bold"> Everyone</span>
               <span>{dropdown}</span>
             </div>
-          </div>
+          </div> */}
           <div className="py-[11px]">
             <textarea
               ref={textareaRef}
@@ -80,10 +88,10 @@ export default function Post({ setShouldUpdate }): JSX.Element {
           )}
         </div>
 
-        <div className="flex flex-row text-[13px] font-bold leading-[15px] pb-[11px]">
+        {/* <div className="flex flex-row text-[13px] font-bold leading-[15px] pb-[11px]">
           <div>{globe}</div>
           <span className="text-blue"> Everyone can reply</span>
-        </div>
+        </div> */}
 
         <div className="flex flex-row justify-between pt-[9px] ml-[-8px]">
           <div className="flex flex-row">
@@ -119,10 +127,10 @@ export default function Post({ setShouldUpdate }): JSX.Element {
           </div>
           <div>
             <button
-              style={{ opacity: postValue || imgFile ? 1 : 0.5 }}
+              style={{ opacity: postValue.trim() || imgFile ? 1 : 0.5 }}
               className="bg-blue min-w-[34px] min-h-[34px] opacity-50 flex px-[15px] ml-11px rounded-full"
               onClick={() => {
-                if (postValue || imgFile) handlePost();
+                if (postValue.trim() || imgFile) void handlePost();
               }}
             >
               <div className="btn-tweet">
