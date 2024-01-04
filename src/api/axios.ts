b88f34e76812af16c00ed390f351fb5611f502f9
lc <gingeraleisthebest@gmail.com>
2024-01-04 17:04:09 -0500

    fixed env var name

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
+
 // const BASE_URL = 'http://localhost:8000/api';
-const BASE_URL = process.env.API_URL;
+const BASE_URL = import.meta.env.VITE_APP_API_URL;
 
 export default axios.create({
   baseURL: BASE_URL,
